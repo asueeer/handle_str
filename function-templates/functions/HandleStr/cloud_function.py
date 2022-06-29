848b97f8577302f2b5fbf436c7293d5ce66e8a1e
asu <asueeer@163.com>
2022-06-30 01:46:06 +0800

    use json load body

--- function-templates/functions/HandleStr/cloud_function.py
+++ function-templates/functions/HandleStr/cloud_function.py
@@ -1,11 +1,13 @@
 from do_handle import *
+import json
 
 
 def cloud_function(json_input):
-    s = json_input["inputStr"]
-    p = json_input["pattern"]
-    w = json_input["wordNum"]
-    m = json_input["threshold"]
+    body = json.loads(json_input.get('body'))
+    s = body.get('inputStr')
+    p = body.get('pattern')
+    w = int(body.get('wordNum'))
+    m = int(body.get('threshold'))
 
     # Processing
     count, next_s = handle_str(s, p, w, m)
