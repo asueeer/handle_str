1f36095cb7d11b321be62d5ffbe98d335a1068c6
asu <asueeer@163.com>
2022-06-29 23:37:24 +0800

    add function templates files

---
+++ function-templates/functions/template-node/docker-runtime.js
@@ -0,0 +1,15 @@
+var myF = require('./index.js');
+const fs = require('fs');
+let rawdata = fs.readFileSync('./input.json');
+const event = {
+    body: rawdata
+  };
+
+
+myF.handler(event).
+  then((result) => {
+    console.log(result);
+  }).
+  catch(err => {
+    console.log(err);
+  });
